dbbfde3f361454c9fbcb0f6517a7bbcd670e1b7e
justinterada <jtvkws001@gmail.com>
2018-03-20 21:21:42 -0500

    Connect to the database

--- src/main/java/org/jcomputing/Application.java
+++ src/main/java/org/jcomputing/Application.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.sql.*;
+
 @Controller
 @EnableAutoConfiguration
 public class Application {
@@ -14,6 +16,35 @@ public class Application {
     @ResponseBody
     String home() {
         return "Hello World!";
+    }
+
+    @RequestMapping("/teams")
+    @ResponseBody
+    String teams() {
+        String query = "select * from 2018_team";
+        Connection con = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+
+        StringBuilder builder = new StringBuilder();
+
+        try {
+            // opening database connection to MySQL server
+            con = DriverManager.getConnection("jdbc:mysql://ncaatournamentpool.cvksgnszabxj.us-east-2.rds.amazonaws.com:3306/ebdb",
+                    "user", "password");
+            stmt = con.createStatement();
+            rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                builder.append(rs.getString("name") + "<br />");
+            }
+        } catch (SQLException sqlEx) { sqlEx.printStackTrace(); }
+        finally { //close connection ,stmt and resultset here
+            try { con.close(); } catch(SQLException se) { /*can't do anything */ }
+            try { stmt.close(); } catch(SQLException se) { /*can't do anything */ }
+            try { rs.close(); } catch(SQLException se) { /*can't do anything */ }
+        }
+
+        return builder.toString();
     }
 
     public static void main(String[] args) throws Exception {
